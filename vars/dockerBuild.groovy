819886a7549af7f628887ebf7af2d48dae2fb74a
Anurag Lohar <109411443+Githubak2002@users.noreply.github.com>
2025-10-06 16:50:55 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,3 +1,4 @@
 def call(String ImageName, String ImageTag, String DockerHubUser) {
+    echo "Creating docker build (docker image)"
     sh "docker build -t ${DockerHubUser}/${ImageName}:${ImageTag} ."
 }
