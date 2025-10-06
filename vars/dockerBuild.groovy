6b373994d644c7ea40e420243c506f2ab558a41c
Anurag Lohar <109411443+Githubak2002@users.noreply.github.com>
2025-10-06 16:48:58 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,6 @@
 def call(String ImageName, String ImageTag, String DockerHubUser) {
     sh """
-        export DOCKER_BUILDKIT=1
+        // export DOCKER_BUILDKIT=1
         docker build -t ${DockerHubUser}/${ImageName}:${ImageTag} .
     """
 }
